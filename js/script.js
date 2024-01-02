fd28fb380407934c01f642bdfc10754b541485c6
Prokash Pul <pprokash102@gmail.com>
2024-01-02 23:19:33 +0600

    scroll active javascript add

--- js/script.js
+++ js/script.js
@@ -2,8 +2,28 @@
 
 
 // scroll section
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("nav a");
 window.onscroll = () => {
+    // scrool active
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(
+                links => {
+                    links.classList.remove("active");
+                    document.querySelector("nav a[href*=" + id + "]").classList.add("active")
+                }
+            )
+        }
+    })
+
+
     // scroll header
     let nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 100)
+    nav.classList.toggle("sticky", window.scrollY > 100);
 }
